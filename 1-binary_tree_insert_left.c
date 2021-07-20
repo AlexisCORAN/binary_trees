1964b1b766c3e1171c55858c03997620a8d51ba4
Manuel-condori <2828@holbertonschool.com>
2021-07-20 18:41:38 -0500

    add coment to file 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		node_child->left = parent->left;
 		parent->left->parent = node_child;
+		parent->left = node_child;
 	}
 	return(node_child);
 }
