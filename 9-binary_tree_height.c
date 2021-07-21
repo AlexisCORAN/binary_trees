8e1720591a829ec4c669ceab35eedc1266b483d9
AlexisCORAN <alexis_ca_@outlook.com>
2021-07-21 15:58:52 +0000

    Fixed typo for task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,10 @@
-#include "binary_trees.h"i
+#include "binary_trees.h"
 
 /**
- * binary_tree_height - Entry point
+ * binary_tree_height - function that measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  *
- * Return: height
+ * Return: height of the binary tree.
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
