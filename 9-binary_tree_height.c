ec5c5a2707714e6b1e9d435b0ab6881bbc2d6c00
Manuel-condori <2828@holbertonschool.com>
2021-07-21 02:31:50 -0500

    add coments

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,12 @@
-#include "binary_trees.h"
+#include "binary_trees.h"i
+
+/**
+ * binary_tree_height - Entry point
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height
+*/
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left = 0;
