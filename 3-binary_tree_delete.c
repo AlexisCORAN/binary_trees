0dedc4dc240e13a6058bbb792d2a28db90aa60ea
Manuel-condori <2828@holbertonschool.com>
2021-07-21 02:24:44 -0500

    add coments

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - Entry point
+ * @tree: pointer to the root node of the tree to delete
+ * Return: void
+*/
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
