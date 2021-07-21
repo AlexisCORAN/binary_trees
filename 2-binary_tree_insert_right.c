0470b29db401d1dc827261da2136d7b61692790a
AlexisCORAN <alexis_ca_@outlook.com>
2021-07-21 00:05:04 +0000

    Added task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node_child;
+	
+	if(parent == NULL)
+		return (NULL);
+	
+	node_child = binary_tree_node(parent, value);
+	
+
+	if(parent->right == NULL)
+	{
+		parent->right = node_child;
+	}
+	else
+	{
+		node_child->right = parent->right;
+		parent->right->parent = node_child;
+		parent->right = node_child;
+	}
+	return(node_child);
+}
