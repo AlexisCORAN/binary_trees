90ed2f84c6c96327a7ab39d192eef5f0e8027d08
Manuel-condori <2828@holbertonschool.com>
2021-07-21 02:33:22 -0500

    add coments

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - Entry point
+ * @tree: pointer to the root node of the tree to measure the depth
+ * Return: depth
+*/
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
